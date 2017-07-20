26ef1d595df831f3a5611de35e678e3eaa0ced32
Michelle <mbojorquez4@cnm.edu>
2017-07-20 02:56:14 -0600

    fixed spelling error

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,18 +4,25 @@
 	</head>
 	<body>
 		<h1>Jasmine</h1>
+		<img src="image/chef.jpg">
+		<p>Purpose: To expose my professional talents to a broader audience.
+
+			Audience: This project is to attract bussiness owners who need a web developer or a company that is hiring a web developer.
+
+			Goal: The goal is to get a job in web design.</p>
 
 		<p>Jasmine is new to New Mexico and needs help designing
 		her website for her bussisness. She is a personal chef and recently moved from Seattle were she was one of Bill Gate's chef. She is excited about New Mexico
 		but needs a website to refer her potentials clients and to showcase her work and experience</p>
+		</body>
 
 		<table>
-			<tr><th>Frstrations</th>
+			<tr><th>Frustrations</th>
 				<th>Need to know</th></tr>
 			<tr></tr>
 			<tr>
 				<td>Has limited time to find a person to do her website. Doesn't know many people. Is not a technical person.</td>
-				<td>Jasmine likes the idea of having her own website but doesnt even know where to start.
+				<td>Jasmine likes the idea of having her own website but doesn't know where to start.
 				This is Jasmine's first website</td>
 			</tr>
 		</table>
@@ -34,18 +41,17 @@
 		</ol>
 		<h2>Use case</h2>
 		<ol>
-			<li>Amber will use this website to make extra money and get the attention of a new audience</li>
+			<li>Jasmine will use this website to start her new business and get the attention of a new audience.Jasmine was looked over many websites to help her do this.
+			She likes mine in particular because it struck her as chich and professional.
+			She plans on meeting with me as soon as possible to start her own web page.</li>
 		</ol>
 		<h2>Ineraction</h2>
 		<ol>
-			<li>Amber goes to webpage</li>
-			<li>browser loads webpage home/login page</li>
-			<li>Amber creates a profile page for her store.</li>
-			<li>Amber creates a user account.</li>
-			<li>Amber uploads picture of her product.</li>
-			<li>Amber creates a description of her products along with the prices.</li>
-			<li>Amber creates options for method of payment and delivery.</li>
-			<li>Amber creates a hyperlink to take user page to a profile page telling us about herself.</li>
+			<li>Jasmine goes to webpage</li>
+			<li>browser loads webpage</li>
+			<li>Jasmine creates a profile for her contact information.</li>
+			<li>Jasmine creates a user account.</li>
+			<li>Jasmine creates a description of her needs in the comments</li>
 		</ol>
 		<h2>Conceptual model</h2>
 		<ul>
@@ -57,23 +63,18 @@
 			<li>profilePhone</li>
 			<li>profileSalt (account password)</li></ul>
 		<ul>
-			<li><strong>item</strong></li>
+			<li><strong>website</strong></li>
 			<li>ProfileId</li>
-			<li>itemId</li>
-			<li>itemContent</li>
-			<li>itemPrice</li>
-			<li>shippingOptions</li></ul>
+			<li>websiteId</li>
+			<li>websiteContent</li>
+		</ul>
 		<h2>Relations</h2>
 		<ol>
 			<li>Multiple users can comment on one profile</li>
-			<li>Multiple users can oreder multiple bibs on one profile</li>
-			<li>Muliple user can choose one type of payment and delivery</li>
+			<li>Multiple users can create a account on one profile</li>
+			<li>Muliple user create account contact information.</li>
 		</ol>
-		<p>	Scenario
-			Saturday midmorning Amber is doing the weekly family grocery shopping, she receives an alert on her phone about
-			a purchases made on her esty site. She is absolutely thrilled that she can execute selling her product online
-			without it interfering with her daily activities
 
-		</p>
-		<img src="epic/image/diagram5.jpg">
+		</html>
+		<img src="image/Untitled%20Diagram%20(1).jpg">
 
