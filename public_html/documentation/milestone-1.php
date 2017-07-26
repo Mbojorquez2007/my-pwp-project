5644752ed9bb3b5fa96ebfaf13b20d5c19f71fe6
Michelle <mbojorquez4@cnm.edu>
2017-07-26 04:15:35 -0600

    got rid of some unneeded spaces

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,55 +1,54 @@
-	<html>
-			<head><meta charset="utf-8"/>
-				<title>milestone-1</title>
-			</head>
-			<body>
-				<h1>Jasmine</h1>
-				<img src="image/chef.jpg">
-				<p>Purpose: To expose my professional talents to a broader audience.
+ <!DOCTYPE html>
+<html>
+	<head><meta charset="utf-8"/>
+		<title>milestone-1</title>
+	</head>
+	<body>
+		<h1>Jasmine</h1>
+		<img src=  "image/chef.jpg">
+		<p>Purpose: To expose my professional talents to a broader audience.
+			Audience: This project is to attract business owners who need a web developer or a company that is hiring a web developer.
+			Goal: The goal is to get a job in web design.</p>
+		<p>Jasmine is new to New Mexico and needs help designing
+			her website for her business. She is a personal chef and recently moved from Seattle were she was one of Bill Gate's chef. She is excited about New Mexico
+			but needs a website to refer her potentials clients and to showcase her work and experience</p>
 
-					Audience: This project is to attract bussiness owners who need a web developer or a company that is hiring a web developer.
+		<table>
+			<tr>
 
-					Goal: The goal is to get a job in web design.</p>
-
-				<p>Jasmine is new to New Mexico and needs help designing
-					her website for her bussisness. She is a personal chef and recently moved from Seattle were she was one of Bill Gate's chef. She is excited about New Mexico
-					but needs a website to refer her potentials clients and to showcase her work and experience</p>
-			</body>
-
-			<table>
-				<tr><th>Frustrations</th>
-					<th>Need to know</th></tr>
-					<tr>
-						<td>Has limited time to find a person to do her website. Doesn't know many people. Is not a technical person.</td>
-					<td>Jasmine likes the idea of having her own website but doesn't know where to start. This is Jasmine's first website</td>
-				</tr>
-			</table>
+				<th>Frustrations</th>
+				<th>Need to know</th></tr>
+			<tr></tr>
+			<tr>
+				<td>Has limited time to find a person to do her website. Doesn't know many people. Is not a technical person.</td>
+				<td>Jasmine likes the idea of having her own website but doesn't know where to start. This is Jasmine's first website</td>
+			</tr>
+		</table>
 
 			<!-- This is an HTML comment -->
-			<h2>Purpose</h2>
-			<ul>
-				<li>take her business to another level</li>
-				<li>attract new clients that she may not reach otherwise</li>
-				<li>Showcase her work and experience</li>
-			</ul>
-
-			<h2>Technology</h2>
-			<ol>
-				<li>Apple iMac(Latest Model)- Intel Core i5 home computer, iphone7 when she is on the go</li>
-			</ol>
+		<h2>Purpose</h2>
+		<ul>
+			<li>take her business to another level</li>
+			<li>attract new clients that she may not reach otherwise</li>
+			<li>Showcase her work and experience</li>
+		</ul>
+		<h2>Technology</h2>
+		<ol>
+			<li>Apple iMac(Latest Model)- Intel Core i5 home computer, iphone7 when she is on the go</li>
+		</ol>
 			<h2>Use case</h2>
-			<ol>
-				<li>Jasmine will use this website to start her new business and get the attention of a new audience.Jasmine was looked over many websites to help her do this.
-					She likes mine in particular because it struck her as chich and professional.
-					She plans on meeting with me as soon as possible to start her own web page.</li>
-			</ol>
+		<ol>
+			<li>Jasmine will use this website to start her new business and get the attention of a new audience.Jasmine was looked over many websites to help her do this.
+				She likes mine in particular because it struck her as chich and professional.
+				She plans on meeting with me as soon as possible to start her own web page.</li>
+		</ol>
 			<h2>Ineraction</h2>
-			<ol>
-				<li>Jasmine goes to webpage</li>
-				<li>browser loads webpage</li>
-				<li>Jasmine creates a profile for her contact information.</li>
-				<li>Jasmine creates a user account.</li>
-				<li>Jasmine creates a description of her needs in the comments</li>
-			</ol>
-
-		</html>
+		<ol>
+			<li>Jasmine goes to webpage</li>
+			<li>browser loads webpage</li>
+			<li>Jasmine creates a profile for her contact information.</li>
+			<li>Jasmine creates a user account.</li>
+			<li>Jasmine creates a description of her needs in the comments</li>
+		</ol>
+	</body>
+ </html>
