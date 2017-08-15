30de9882db2682c63ce416d923293c3bf3e4658e
Michelle <mbojorquez4@cnm.edu>
2017-08-15 08:26:49 -0600

    fixing errors

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,14 +12,14 @@
 		<p>This will have my photo a label saying Hi Im a web developer and i will have a content bar
 			that will allow users to go to different sections.</p>
 		<h1>The About Me section</h1>
-		<p>A detailed section about who I am and what I do aglong with an image.
+		<p>A detiled section about who I am and what I do aglong with an image.
 		<h1>Portfolio</h1>
 		<p>This section will have pictures of my previous projects with title and description of the project underneath.</p>
 		<h1>The Contact me section</h1>
 		<p>This section will have a contact me form where people can put in name, email and phone and message and ask questions and reach me on social media</p>
-		<img src="documentation/image/mock1.jpg" alt="mock1">
-		<img src="documentation/image/mock2.jpg" alt="mock2">
-		<img src="doccumenation/image/mock3.jpg" alt="mock3">
-		<img src="documentation/image/mock4.jpg" alt="mock4">
+		<img src="image/mock1.jpg" alt="mock1">
+		<img src="image/mock2.jpg" alt="mock2">
+		<img src="image/mock3.jpg" alt="mock3">
+		<img src="image/mock4.jpg" alt="mock4">
 	</body>
 
