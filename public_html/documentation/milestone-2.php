0b29e1680a0a439341fafafaa893efb737efc3cd
Michelle <mbojorquez4@cnm.edu>
2017-08-14 18:37:37 -0600

    fixing errors

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,4 +1,4 @@
-<!DOCTYPE_html>
+<!DOCTYPEhtml>
 <html>
 	<head>
 		<meta charset="utf-8"/>
@@ -16,10 +16,10 @@
 		<h1>Portfolio</h1>
 		<p>This section will have pictures of my previous projects with title and description of the project underneath.</p>
 		<h1>The Contact me section</h1>
-		<p>This section will have a contact me form where people can put in name, email and phone and message and ask questions to me directly to contact me directly</p>
-		<img src="image/mock1.jpg" alt="mock1">
-		<img src="image/mock2.jpg" alt="mock2">
-		<img src="image/mock3.jpg" alt="mock3">
-		<img src="image/mock4.jpg" alt="mock4">
+		<p>This section will have a contact me form where people can put in name, email and phone and message and ask questions and reach me on social media</p>
+		<img src="documentation/image/mock1.jpg" alt="mock1">
+		<img src="documentation/image/mock2.jpg" alt="mock2">
+		<img src="doccumenation/image/mock3.jpg" alt="mock3">
+		<img src="documentation/image/mock4.jpg" alt="mock4">
 	</body>
 
