e80d676133af48fb8d59a66aa8b8eef0968d47f9
Michelle <mbojorquez4@cnm.edu>
2017-08-22 07:29:44 -0600

    adding more stuff

---
+++ public_html/documentation/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Michelle Allen</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<h2>Circle</h2>
+			<p>The .img-circle class shapes the image to a circle (not available in IE8):</p>
+			<img src="image/meme.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236">
+		</div>
+
+		<div class="jumbotron text-center">
+			<h1>Michelle Allen</h1>
+			<p> Hi!I am a web developer</p>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<h3>Circle</h3>
+					<p>The .img-circle class shapes the image to a circle (not available in IE8):</p>
+					<img src="image/meme.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236">
+				</div>
+				<div class="col-sm-4">
+					<h3>Column 2</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+				</div>
+				<div class="col-sm-4">
+					<h3>Column 3</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,4 +1,4 @@
-<!DOCTYPEhtml>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8"/>
@@ -9,6 +9,10 @@
 		<p>My stratagy is to format my pwp to allow people to see my name profile and showcase my work</p>
 		<p>My site will have a way to contact me and ask questions</p>
 		<h1>Head Piece</h1>
+		<img src="image/mock1.jpg" alt="mock1">
+		<img src="image/mock2.jpg" alt="mock2">
+		<img src="image/mock3.jpg" alt="mock3">
+		<img src="image/mock4.jpg" alt="mock4">
 		<p>This will have my photo a label saying Hi Im a web developer and i will have a content bar
 			that will allow users to go to different sections.</p>
 		<h1>The About Me section</h1>
@@ -17,9 +21,6 @@
 		<p>This section will have pictures of my previous projects with title and description of the project underneath.</p>
 		<h1>The Contact me section</h1>
 		<p>This section will have a contact me form where people can put in name, email and phone and message and ask questions and reach me on social media</p>
-		<img src="image/mock1.jpg" alt="mock1">
-		<img src="image/mock2.jpg" alt="mock2">
-		<img src="image/mock3.jpg" alt="mock3">
-		<img src="image/mock4.jpg" alt="mock4">
 	</body>
+</html>
 
