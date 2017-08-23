893b25f2a9f40292b95d1750e6236d4d59bea0fb
Michelle <mbojorquez4@cnm.edu>
2017-08-23 00:56:46 -0600

    adding more

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -1,44 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Michelle Allen</title>
+
+		<!-- Required meta tags -->
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	</head>
+	<div class="container" style="background-image:('.meme.jpg');">
+		...
+	</div>
+
 	<body>
-		<div class="container">
-			<h2>Circle</h2>
-			<p>The .img-circle class shapes the image to a circle (not available in IE8):</p>
-			<img src="image/meme.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236">
-		</div>
 
 		<div class="jumbotron text-center">
+			<div class="container-fluid">
 			<h1>Michelle Allen</h1>
-			<p> Hi!I am a web developer</p>
-		</div>
-
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-4">
-					<h3>Circle</h3>
-					<p>The.img-circle class shapes the image to a circle (not available in IE8):</p>
+			<p>Hi I am a web developer</p>
+				<div class="container">
+					<h2>Circle</h2>
+					<p>The .img-circle class shapes the image to a circle (not available in IE8):</p>
 					<img src="image/meme.jpg" class="img-circle" alt="me" width="358" height="638">
 				</div>
-				<div class="col-sm-4">
-					<h3>Circle</h3>
-					<p>The.img-circle class shapes the image to a circle (not available in IE8).</p>
-					<img src="image/michelle.jpg" class="img-circle" alt="michelle" width="358" height="638">
+				<div class="row">
+					<div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+
+		<div class="container">
+			<h2>Image Gallery</h2>
+			<p>The .thumbnail class can be used to display an image gallery.</p>
+			<p>The .caption class adds proper padding and a dark grey color to text inside thumbnails.</p>
+			<p>Click on the images to enlarge them.</p>
+			<div class="row">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<a href="/w3images/lights.jpg" target="_blank">
+							<img src="/w3images/lights.jpg" alt="Lights" style="width:100%">
+							<div class="caption">
+								<p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+							</div>
+						</a>
+					</div>
 				</div>
-				<div class="col-sm-4">
-					<h3>Circle</h3>
-					<p>The.img-circle class shapes the image to a circle (not available in IE8).</p>
-					<img src="image/meme.jpg" class="img-circle" alt="Cinque Terre" width="358" height="638">
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<a href="/w3images/nature.jpg" target="_blank">
+							<img src="/w3images/nature.jpg" alt="Nature" style="width:100%">
+							<div class="caption">
+								<p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="thumbnail">
+						<a href="/w3images/fjords.jpg" target="_blank">
+							<img src="/w3images/fjords.jpg" alt="Fjords" style="width:100%">
+							<div class="caption">
+								<p>Lorem ipsum donec id elit non mi porta gravida at eget metus.</p>
+							</div>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
+	</body>
+</html>
 
+<h1>Hello, world!</h1>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	</body>
 </html>
